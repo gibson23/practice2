783260a288720df810c34e38abcd34b8140337c9
hypson <hypson@yandex.ru>
2016-04-10 13:53:48 +0300

    Initial commit of sixth lab work

---
+++ Practice/src/laba6/IOUtilsImpl.java
@@ -0,0 +1,184 @@
+package laba6;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
+
+import interfaces.task6.IOUtils;
+
+public class IOUtilsImpl implements IOUtils {
+
+	@Override
+	public void copyFile(String source, String dest) {
+		if (source == null || dest == null)
+			throw new NullPointerException();
+		if (source.equalsIgnoreCase(dest))
+			throw new IllegalArgumentException("Can not copy to the same file");
+
+		try (InputStream in = new FileInputStream(source);
+				OutputStream out = new FileOutputStream(dest)) {
+			int k;
+			while ((k = in.read()) != -1) {
+				out.write(k);
+			}
+		} catch (FileNotFoundException | SecurityException e) {
+			throw new IllegalArgumentException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void copyFileBest(String source, String dest) {
+
+		FileChannel sourceChannel = null;
+		FileChannel destChannel = null;
+
+		try {
+			sourceChannel = new FileInputStream(source).getChannel();
+			destChannel = new FileOutputStream(dest).getChannel();
+			sourceChannel.transferTo(0, sourceChannel.size(), destChannel);
+		} catch (IOException e) {
+			throw new IllegalArgumentException(e);
+		} finally {
+			if (sourceChannel != null) {
+				try {
+					sourceChannel.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (destChannel != null) {
+				try {
+					destChannel.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void copyFileBuffered(File source, File dest) {
+		if (source == null || dest == null)
+			throw new NullPointerException();
+		if (source.equals(dest))
+			throw new IllegalArgumentException("Can not copy to the same file");
+
+		try (InputStream in = new BufferedInputStream(new FileInputStream(
+				source));
+				OutputStream out = new BufferedOutputStream(
+						new FileOutputStream(dest))) {
+			int k;
+			while ((k = in.read()) != -1) {
+				out.write(k);
+			}
+		} catch (FileNotFoundException | SecurityException e) {
+			throw new IllegalArgumentException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String[] findFiles(String dir) {
+		if (dir == null)
+			throw new NullPointerException();
+
+		File path = new File(dir);
+		if (!path.isDirectory())
+			throw new IllegalArgumentException();
+		File[] fileArr = path.listFiles();
+		List<String> list = new ArrayList<>();
+		for (File file : fileArr) {
+			if (file.isFile()) {
+				list.add(file.getAbsolutePath());
+			} else {
+				list.addAll(Arrays.asList(findFiles(file.getAbsolutePath())));
+			}
+		}
+		String[] result = new String[list.size()];
+		return list.toArray(result);
+	}
+
+	@Override
+	public String[] findFiles(String dir, String extention) {
+
+		if (extention == null)
+			return findFiles(dir);
+		if (dir == null)
+			throw new NullPointerException();
+
+		File path = new File(dir);
+		if (!path.isDirectory())
+			throw new IllegalArgumentException();
+		File[] fileArr = path.listFiles();
+		List<String> list = new ArrayList<>();
+		for (File file : fileArr) {
+			if (file.isFile()) {
+				if(file.getName().endsWith(extention)) {
+					list.add(file.getAbsolutePath());
+				}
+			} else {
+				list.addAll(Arrays.asList(findFiles(file.getAbsolutePath(), extention)));
+			}
+		}
+		String[] result = new String[list.size()];
+		return list.toArray(result);
+	}
+
+	@Override
+	public void replaceChars(Reader in, Writer out, String inChars,
+			String outChars) {
+		
+		if(in == null || out == null) throw new NullPointerException();
+		if(inChars.length() != outChars.length())
+			throw new IllegalArgumentException();
+		
+		Map<Character, Character> map = new HashMap<>();
+		char[] inArr = inChars.toCharArray();
+		char[] outArr = outChars.toCharArray();
+		for (int i = 0; i < outArr.length; i++) {
+			map.put(inArr[i], outArr[i]);
+		}
+		
+		int k;
+		try {
+			while ((k = in.read()) != -1) {
+				
+				out.write(map.containsKey((char)k) ? map.get((char)k) : k);
+			}
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} finally {
+			try {
+				out.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+
+	}
+
+}
---
+++ Practice/src/laba6/Test.java
@@ -0,0 +1,53 @@
+package laba6;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+import interfaces.task6.IOUtils;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		File sourceFile = new File("D:\\books\\kniga.pdf");
+		String sourceString = "D:\\books\\kniga.pdf";
+
+		IOUtils utils = new IOUtilsImpl();
+		long start = System.currentTimeMillis();
+
+		utils.copyFileBuffered(sourceFile, new File("D:\\books\\1.pdf"));
+		System.out.println("time of copyFileBuffered() is "
+				+ (System.currentTimeMillis() - start));
+
+		start = System.currentTimeMillis();
+		utils.copyFile(sourceString, "D:\\books\\2.pdf");
+		System.out.println("time of copyFile() is "
+				+ (System.currentTimeMillis() - start));
+
+		start = System.currentTimeMillis();
+		utils.copyFileBest(sourceString, "D:\\books\\3.pdf");
+		System.out.println("time of copyFileBest() is "
+				+ (System.currentTimeMillis() - start));
+
+		for (String s : utils.findFiles("D:\\films")) {
+			System.out.println(s);
+		}
+
+		System.out.println("-------------------------");
+
+		for (String s : utils.findFiles(
+				"C:\\Users\\Vasya\\git\\practice2\\Practice", ".java")) {
+			System.out.println(s);
+		}
+
+		Reader in = new FileReader("D:\\from.txt");
+		Writer out = new FileWriter("D:\\to.txt");
+
+		utils.replaceChars(in, out, "mo-.", "pa^!");
+
+	}
+
+}
