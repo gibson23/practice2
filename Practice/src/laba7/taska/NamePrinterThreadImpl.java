7a3f1d1019296f3ff7ab6c521432798612fdfba6
hypson <hypson@yandex.ru>
2016-04-12 14:07:48 +0300

    Initial commit of the first part of 7th lab work.

---
+++ Practice/src/laba7/taska/NamePrinterThreadImpl.java
@@ -0,0 +1,52 @@
+package laba7.taska;
+
+import java.io.PrintStream;
+
+import interfaces.task7.simple.NamePrinterThread;
+
+public class NamePrinterThreadImpl extends NamePrinterThread {
+	
+	private int count;
+	private long interval;
+	private String name;
+	private PrintStream stream;
+
+	public NamePrinterThreadImpl() {
+		
+	}
+	
+	@Override
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public void setInterval(long ms) {
+		this.interval = ms;
+	}
+
+	@Override
+	public void setPrintName(String name) {
+		this.name = name;
+
+	}
+
+	@Override
+	public void setStream(PrintStream stream) {
+		this.stream = stream;
+	}
+
+	@Override
+	public void run() {
+		stream.print(name);
+		while (--count > 0) {
+			try {
+				Thread.sleep(interval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			stream.print(name);
+		}
+
+	}
+}
---
+++ Practice/src/laba7/taska/Test.java
@@ -0,0 +1,27 @@
+package laba7.taska;
+
+import interfaces.task7.simple.NamePrinterRunnable;
+import interfaces.task7.simple.NamePrinterThread;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		NamePrinterRunnable runnImpl = new NamePrinterRunnableImpl();
+		runnImpl.setCount(5);
+		runnImpl.setInterval(2000);
+		runnImpl.setPrintName("Mihail");
+		runnImpl.setStream(System.out);
+		Thread thread1 = new Thread(runnImpl);
+		thread1.start();
+		NamePrinterThread thread2 = new NamePrinterThreadImpl();
+		thread2.setCount(8);
+		thread2.setInterval(1000);
+		thread2.setPrintName("Ibragim");
+		thread2.setStream(System.out);
+		thread2.start();
+
+	}
+
+}
