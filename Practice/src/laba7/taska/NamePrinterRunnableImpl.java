cea0d332e53c4fa1e18cadeb5ec8ae73c828c87a
hypson <hypson@yandex.ru>
2016-04-15 10:24:46 +0300

    Correct mistakes in lab 7 task A

--- Practice/src/laba7/taska/NamePrinterRunnableImpl.java
+++ Practice/src/laba7/taska/NamePrinterRunnableImpl.java
@@ -12,40 +12,49 @@ public class NamePrinterRunnableImpl implements NamePrinterRunnable {
 	private PrintStream stream;
 
 	public NamePrinterRunnableImpl() {
-
+		
 	}
 
 	@Override
 	public void setCount(int count) {
+		if(count <= 0)
+			throw new IllegalArgumentException("count should be 1 or more");
 		this.count = count;
 	}
 
 	@Override
 	public void setInterval(long ms) {
+		if(ms <= 0)
+			throw new IllegalArgumentException("ms should be 1 or more");
 		this.interval = ms;
 	}
 
 	@Override
 	public void setPrintName(String name) {
+		if(name == null) throw new NullPointerException();
+		if(name.length() == 0) throw new IllegalArgumentException("empty name");
 		this.name = name;
 
 	}
 
 	@Override
 	public void setStream(PrintStream stream) {
+		if(stream == null) throw new NullPointerException();
 		this.stream = stream;
 	}
 
 	@Override
 	public void run() {
-		stream.print(name);
+		Thread.currentThread().setName(name);
+		
+		stream.print(Thread.currentThread().getName());
 		while (--count > 0) {
 			try {
 				Thread.sleep(interval);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			stream.print(name);
+			stream.print(Thread.currentThread().getName());
 		}
 
 	}
--- Practice/src/laba7/taska/NamePrinterThreadImpl.java
+++ Practice/src/laba7/taska/NamePrinterThreadImpl.java
@@ -5,47 +5,55 @@ import java.io.PrintStream;
 import interfaces.task7.simple.NamePrinterThread;
 
 public class NamePrinterThreadImpl extends NamePrinterThread {
-	
+
 	private int count;
 	private long interval;
-	private String name;
 	private PrintStream stream;
 
 	public NamePrinterThreadImpl() {
-		
+
 	}
-	
+
 	@Override
 	public void setCount(int count) {
+		if (count <= 0)
+			throw new IllegalArgumentException("count should be 1 or more");
 		this.count = count;
 	}
 
 	@Override
 	public void setInterval(long ms) {
+		if (ms <= 0)
+			throw new IllegalArgumentException("ms should be 1 or more");
 		this.interval = ms;
 	}
 
 	@Override
 	public void setPrintName(String name) {
-		this.name = name;
-
+		if (name == null)
+			throw new NullPointerException();
+		if (name.length() == 0)
+			throw new IllegalArgumentException("empty name");
+		setName(name);
 	}
 
 	@Override
 	public void setStream(PrintStream stream) {
+		if (stream == null)
+			throw new NullPointerException();
 		this.stream = stream;
 	}
 
 	@Override
 	public void run() {
-		stream.print(name);
+		stream.print(getName());
 		while (--count > 0) {
 			try {
 				Thread.sleep(interval);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			stream.print(name);
+			stream.print(getName());
 		}
 
 	}
