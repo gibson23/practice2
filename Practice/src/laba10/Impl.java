d271103476b175fad6cbbbd1c2705b1a8485476f
Vasiliy <tokse23@gmail.com>
2016-04-18 16:57:24 +0300

    Add junit with src to classpath

---
+++ Practice/src/laba10/Impl.java
@@ -0,0 +1,14 @@
+package laba10;
+
+import interfaces.junit.JunitTester;
+import junit.framework.TestSuite;
+
+public class Impl implements JunitTester {
+
+	@Override
+	public TestSuite suite() {
+
+		return null;
+	}
+
+}
